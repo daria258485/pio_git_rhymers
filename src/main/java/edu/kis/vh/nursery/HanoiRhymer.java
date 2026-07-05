09d9764bd2b01c700b164c660cda1557cd419e46
daria258485 <258485@edu.p.lodz.pl>
2026-07-06 00:57:46 +0200

    18. dodanie notatek TODO wskazujacych miejsca do poprawy

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -9,6 +9,9 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     }
     @Override
     public void countIn(int in) {
+        // TODO: metoda po cichu odrzuca niepoprawne wartosci - warto rozwazyc
+        // zwracanie informacji o odrzuceniu (np. boolean) albo rzucenie wyjatku,
+        // zamiast tylko zwiekszac licznik totalRejected
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
