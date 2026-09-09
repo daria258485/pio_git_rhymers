29857bc95e9bda0fd1a471637e58a7ea5d929e9e
daria258485 <258485@edu.p.lodz.pl>
2026-09-09 12:23:21 +0200

    poprawki z code review: RhymersFactory, nazwy metod, hermetyzacja Node

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -39,12 +39,32 @@ public class IntLinkedList {
 
     private static class Node {
 
-        protected final int value;
-        protected Node prev, next;
+        private final int value;
+        private Node prev;
+        private Node next;
 
         public Node(int i) {
             value = i;
         }
 
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 }
